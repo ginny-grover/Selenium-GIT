25cba890cc8879a700f30f5264117318270c1bcb
ginny.grover <ginny.grover@DDC2-LX-6021520.dir.svc.accenture.com>
2018-07-18 11:14:59 +0530

    2nd commit

---
+++ LoggingExample/src/Day3/ClassDemo.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class ClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Learning how to commit code in GIT");
+	}
+
+}
